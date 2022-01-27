1846151fc6170c9c01ab7fb4ec33d6848c6efa70
Brandon Chuck <blc42310@gmail.com>
2022-01-26 22:27:53 -0800

    removed data manipulation function from frontend; this logic is now being handled on the backend; Removed logic from TweetList component for now;

--- Twitter-Showcase-WebAPI/frontend/src/components/TweetsList.js
+++ Twitter-Showcase-WebAPI/frontend/src/components/TweetsList.js
@@ -1,23 +1,6 @@
 import React from "react";
 // import axios from "axios";
 
-const TweetsList = ({ tweetResponse }) => {
-  return (
-    <>
-      <div>{tweetResponse && console.log(tweetResponse)}</div>
-      <div>
-        {tweetResponse &&
-          tweetResponse.data.map((tweet) => {
-            return <div key={tweet.id}>{tweet.text}</div>;
-          })}
-
-        {tweetResponse &&
-          tweetResponse.includes.users.map((tweet, index) => {
-            return <div key={index}>{tweet.username}</div>;
-          })}
-      </div>
-    </>
-  );
-};
+const TweetsList = ({}) => {};
 
 export default TweetsList;
