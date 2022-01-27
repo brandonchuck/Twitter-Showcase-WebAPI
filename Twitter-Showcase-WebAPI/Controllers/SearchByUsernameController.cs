a1506eda150ce57699ff84229a0db32a248174fa
Brandon Chuck <blc42310@gmail.com>
2022-01-26 21:50:49 -0800

    Refactored SearchByUsernameController to return a list of custom TweetObjects to the front end instead of passing entire userTimeline to frontend; Working on properly grabbing media attachments and adding them to a TweetObject;

--- Twitter-Showcase-WebAPI/Controllers/SearchByUsernameController.cs
+++ Twitter-Showcase-WebAPI/Controllers/SearchByUsernameController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Cors;
@@ -28,12 +29,75 @@ namespace Twitter_Showcase_WebAPI.Controllers
         }
 
         [HttpGet]
-        public async Task<string> GetUserTimeline([FromQuery] string searchTerm)
+        public async Task<List<TweetObject>> GetUserTimeline([FromQuery] string searchTerm)
         {
             string authToken = await _twitterAuthorizationService.GetBearerToken(_configuration["Twitter:ApiKey"], _configuration["Twitter:SecretKey"]);
             UserDetails userDetails = await _userDetailsService.GetUserId(searchTerm, authToken);
             var userTimeline = await _userTimelineService.GetUserTimeline(userDetails.data.id, authToken);
-            return JsonSerializer.Serialize(userTimeline);
+
+            List<TweetObject> tweetList = new List<TweetObject>();
+
+            var dataArray = userTimeline.data;
+            var includes = userTimeline.includes;
+
+            List<TweetObject> tweets = userTimeline.data.Select<TweetData>(x =>
+            {
+                TweetObject tweet = new TweetObject()
+                {
+                    Text = x.text,
+                    CreatedAt = x.created_at,
+                    LikeCount = x.public_metrics.like_count,
+                    RetweetCount = x.public_metrics.retweet_count,
+                    CommentCount = x.public_metrics.reply_count
+                };
+
+                // find username and name
+                UserData currentUser = userTimeline.includes.users.First(user => {
+                    return user.id == x.id;
+                });
+
+                // set ScreenName and Username for tweet object
+                tweet.ScreenName = currentUser.name;
+                tweet.Username = currentUser.username;
+                tweet.ProfileImageUrl = currentUser.profile_image_url;
+
+                // check if there are any media_keys
+                if (x.attachments.media_keys != null)
+                {
+                    // loop through each media_key in the array
+                    foreach(string key in x.attachments.media_keys)
+                    {
+                        // loop through each media_key in includes.media
+                        foreach (MediaData m in userTimeline.includes.media)
+                        {
+                            // check if these keys match for the current tweet object
+                            if (m.media_key == key)
+                            { 
+                                if (m.type == "photo")
+                                {
+                                    // set photo
+                                    tweet.ImageUrl = m.url;
+                                } 
+
+                                if (m.type == "video")
+                                {
+                                    // set video preview image
+                                    tweet.VideoPreviewImageUrl = m.preview_image_url;
+                                }                                
+                            }
+                        }
+                    }
+                } else
+                {
+                    tweet.ImageUrl = null;
+                    tweet.VideoPreviewImageUrl = null;
+                }
+            });
+
+            return tweetList;
+
+            //return JsonSerializer.Serialize(userTimeline);
         }
+
     }
 }
