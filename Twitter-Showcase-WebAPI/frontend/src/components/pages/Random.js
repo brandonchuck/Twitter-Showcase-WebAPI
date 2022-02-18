59f0a20f687cfde466eb8c1decb7808456241d6e
Brandon Chuck <blc42310@gmail.com>
2022-02-18 14:57:38 -0800

    Fixed issue for profile pictures but MUST be refactored. Current code does NOT follow DRY principles at all; The issue was that the src obviously cannot accept a Promise object from the async function and instead the the promise must be resolved and the image url stored in state before the src tag can accept it in the form of a string;

--- Twitter-Showcase-WebAPI/frontend/src/components/pages/Random.js
+++ Twitter-Showcase-WebAPI/frontend/src/components/pages/Random.js
@@ -1,10 +1,15 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TweetsList from "../TweetsList";
 import { Container, Row, Col } from "react-bootstrap";
 
 const Random = () => {
   const [randomTweets, setRandomTweets] = useState([]);
+  const [user1Pfp, setUser1Pfp] = useState("");
+  const [user2Pfp, setUser2Pfp] = useState("");
+  const [user3Pfp, setUser3Pfp] = useState("");
+  const [user4Pfp, setUser4Pfp] = useState("");
+  const [user5Pfp, setUser5Pfp] = useState("");
 
   const USER_1 = "garyvee";
   const USER_2 = "shanicucic96";
@@ -12,7 +17,38 @@ const Random = () => {
   const USER_4 = "PGodjira";
   const USER_5 = "AbsoluteSaltETH";
 
-  let profilePicture_USER1 = getProfilePictures(USER_1);
+  useEffect(() => {
+    const pfp1 = async (user) => {
+      let pfp = await getProfilePictures(user);
+      setUser1Pfp(pfp);
+    };
+
+    const pfp2 = async (user) => {
+      let pfp = await getProfilePictures(user);
+      setUser2Pfp(pfp);
+    };
+
+    const pfp3 = async (user) => {
+      let pfp = await getProfilePictures(user);
+      setUser3Pfp(pfp);
+    };
+
+    const pfp4 = async (user) => {
+      let pfp = await getProfilePictures(user);
+      setUser4Pfp(pfp);
+    };
+
+    const pfp5 = async (user) => {
+      let pfp = await getProfilePictures(user);
+      setUser5Pfp(pfp);
+    };
+
+    pfp1(USER_1);
+    pfp2(USER_2);
+    pfp3(USER_3);
+    pfp4(USER_4);
+    pfp5(USER_5);
+  }, []);
 
   async function getRandomTweets(e) {
     e.preventDefault();
@@ -22,11 +58,10 @@ const Random = () => {
     setRandomTweets(res.data);
   }
 
-  async function getProfilePictures(username) {
+  async function getProfilePictures(username, usernameState) {
     let res = await axios.get(
       `api/tweets/search/random/profile-picture?username=${username}`
     );
-
     console.log(res.data);
     let profilePicture = res.data.includes.users[0].profile_image_url;
     console.log(profilePicture);
@@ -43,7 +78,7 @@ const Random = () => {
             id="user-1"
             type="image"
             alt="user1"
-            src={profilePicture_USER1}
+            src={user1Pfp}
             value={`${USER_1}`}
             onClick={(e) => getRandomTweets(e)}
           />
@@ -57,7 +92,7 @@ const Random = () => {
             id="user-2"
             type="image"
             alt="user2"
-            src={getProfilePictures(USER_2)}
+            src={user2Pfp}
             // value={`${USER_2}`}
             onClick={(e) => getRandomTweets(e)}
           />
@@ -71,7 +106,7 @@ const Random = () => {
             id="user-3"
             type="image"
             alt="user3"
-            src={`${getProfilePictures(USER_3)}`}
+            src={user3Pfp}
             value={`${USER_3}`}
             onClick={(e) => getRandomTweets(e)}
           />
@@ -85,7 +120,7 @@ const Random = () => {
             id="user-4"
             type="image"
             alt="user4"
-            src={getProfilePictures(USER_4)}
+            src={user4Pfp}
             value={`${USER_4}`}
             onClick={(e) => getRandomTweets(e)}
           />
@@ -99,7 +134,7 @@ const Random = () => {
             id="user-5"
             type="image"
             alt="user5"
-            src={getProfilePictures(USER_5)}
+            src={user5Pfp}
             value={`${USER_5}`}
             onClick={(e) => getRandomTweets(e)}
           />
